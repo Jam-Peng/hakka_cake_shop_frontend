236e7af13bce500ae6644cb122d4630a650b0fd1
Jam Peng <pengzhe.info@gmail.com>
2023-10-29 20:42:09 +0800

    會員中心-個人資料功能 - done

--- src/context/UserProfileContext.jsx
+++ src/context/UserProfileContext.jsx
@@ -1,12 +1,25 @@
-import { createContext, useCallback, useState } from 'react'
+import { createContext, useCallback, useContext, useState } from 'react'
+import { AuthContext } from './AuthContext'
 
 export const UserProfileContext = createContext()
 
 function UserProfileProvider({ children }) {
+  const { currentUser } = useContext(AuthContext)
   const [userOrders, setUserOrders] = useState([])
   const [openOrderItem, setOpenOrderItem] = useState(null)
   const [productsForOrder, setProductsForOrder] = useState([])
-  
+  const [clientProfile, setClientProfile] = useState({})
+
+  const [formAccount, setFormAccount] = useState({
+    updatName: '',
+    newPassword: '',
+    image: null,
+  });
+
+  const [checkPassword, setCheckPassword] = useState('')
+  const [accountMessage, setAccountMessage] = useState('')
+  const [previewImage, setPreviewImage] = useState(null);
+
   const apiurl = "http://127.0.0.1:8000/api/v1"
   
   // 取得所有訂單
@@ -38,15 +51,95 @@ function UserProfileProvider({ children }) {
     } 
   }, []) 
 
+  // 取得會員資料
+  const getClientProfile = useCallback(async () => {
+    const response = await fetch(`${apiurl}/client_profile/${currentUser.user_id}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      }
+    })
+    const data = await response.json()
+    if (response.status === 200) {
+      setClientProfile(data)
+    } 
+  }, [currentUser]) 
+
+  // 更新會員資料
+  const sendUpdateClient = async (e) => {
+    e.preventDefault()
+
+    if (formAccount.newPassword !== checkPassword) {
+      setAccountMessage('請確認密碼是否相同')
+    } else if (formAccount.newPassword < 8) {
+      setAccountMessage('密碼長度最少8碼英數字')
+    } else {
+      const submitData = new FormData();
+      for (const key in formAccount) {
+        submitData.append(key, formAccount[key]);
+      }
+      
+      let response = await fetch(`${apiurl}/client_update/${currentUser.user_id}/`, {
+        method: "PUT",
+        body: submitData
+      })
+      const data = await response.json()
+      if (response.status === 200) {
+        setAccountMessage(data['message'])
+        cancelFormData()
+        removeImage()
+        getClientProfile()
+      } else {
+        setAccountMessage(data['message'])
+      }
+    }
+
+    setTimeout(() => {
+      setAccountMessage('')
+    }, 2000)
+  }
+
+  // 刪除預覽的圖片
+  const removeImage = (index) => {
+    setPreviewImage((prevPreviewImage) => {
+    const newPreviewImage = [...prevPreviewImage];
+    newPreviewImage.splice(index, 1);
+    return newPreviewImage;
+  });
+  // 刪除整個屬性
+  delete formAccount.image
+  };
+  
+  // 清空表單資料
+  const cancelFormData = () => {
+    setFormAccount({
+      updatName: '',
+      newPassword: '',
+      image: null,
+    });
+    setCheckPassword('')
+  }
+
 
   const contextData = {
     userOrders : userOrders,
     openOrderItem : openOrderItem,
     setOpenOrderItem : setOpenOrderItem,
     productsForOrder : productsForOrder,
-    
+    clientProfile : clientProfile,
+    formAccount : formAccount, 
+    setFormAccount : setFormAccount,
+    checkPassword : checkPassword, 
+    setCheckPassword : setCheckPassword,
+    accountMessage : accountMessage, 
+    previewImage : previewImage,
+    setPreviewImage : setPreviewImage,
+
     getOrders : getOrders,
-    getProductsForOrder : getProductsForOrder,
+    getProductsForOrder: getProductsForOrder,
+    getClientProfile : getClientProfile,
+    sendUpdateClient: sendUpdateClient,
+    removeImage : removeImage,
   }
 
   return (
--- src/pages/user_profile/UserAccount.jsx
+++ src/pages/user_profile/UserAccount.jsx
@@ -1,14 +1,171 @@
-import { useContext } from "react"
-import { AuthContext } from "../../context/AuthContext"
+import user_img from "../../assets/user.png"
+import { BsFillPersonVcardFill, BsLockFill } from "react-icons/bs"
+import { LuShieldCheck } from "react-icons/lu"
+import { BiSolidEditAlt } from "react-icons/bi";
+import { useContext, useEffect } from "react"
+import { UserProfileContext } from "../../context/UserProfileContext"
 
 function UserAccount() {
   document.title = '客家糕粿店 | 會員中心'
-  const { currentUser, authToken } = useContext(AuthContext)
-  console.log(currentUser)
-  console.log(authToken)
+  const { getClientProfile, clientProfile, formAccount, setFormAccount, accountMessage, checkPassword, setCheckPassword,
+          sendUpdateClient, previewImage, setPreviewImage, removeImage } = useContext(UserProfileContext)
+
+  const { username, name, email, image } = clientProfile
+  const imgHostUrl = "http://127.0.0.1:8000"
+  // 處理 onChange同步輸入資料
+  const handleDataChange = (e) => {
+    const { name, value } = e.target;
+    setFormAccount({ ...formAccount, [name]: value  });
+  };
+
+  // onChange 照片同步
+  const handleImage = (e) => {
+    const { name, value, files } = e.target
+    if (files) { 
+      // 處理照片預覽
+      const imageFiles = Array.from(files);
+      // 儲存照片到 formAccount變數中
+      setFormAccount({ ...formAccount, image: e.target.files[0] });
+      
+      // 即時預覽選擇的圖片
+      const imagePreviews = imageFiles.map((file) => ({
+        file,
+        preview:URL.createObjectURL(file)
+      }));
+        setPreviewImage(imagePreviews);
+    } else {
+      setFormAccount((prevFormAccount) => ({
+        ...prevFormAccount,
+        [name]: value,
+      }));
+    }
+  }
+
+  useEffect(() => {
+    getClientProfile()
+  },[getClientProfile])
+
 
   return (
-    <div>會員資料</div>
+    <section>
+      <div className="space-y-6 text-[0.9rem]">
+        <div className="flex flex-col space-y-4 border-b py-4">
+          <div> 
+              {image ?
+                <img src={`${imgHostUrl}${image}`} alt="" className="w-24 h-24 border rounded-md"/> 
+              :
+                <img src={user_img} alt="" className="w-24 h-24 border rounded-md"/> 
+              }
+          </div>
+
+          <div className="flex flex-col space-y-1">
+            <div>
+              <span>帳號：</span> 
+              <span>{username}</span>
+            </div>
+            <div>
+              <span>姓名：</span> 
+              <span>{name || "未提供" }</span>
+            </div>
+            <div>
+              <span>Email：</span>
+              <span>{email}</span>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <form action="" onSubmit={sendUpdateClient}>
+            <div >
+              <div className="space-y-4">
+                <div className="flex items-center space-x-10"> 
+                  <span className="text-lg">修改資料</span>
+                  <div className="flex items-center space-x-4">
+                    <button type="submit" className="btn-md btn-in flex items-center space-x-1">
+                      <BiSolidEditAlt size={18} />
+                      <span>更新</span>
+                    </button>
+                    <div>
+                      <span className="text-rose-500">{accountMessage}</span>
+                    </div>
+                  </div>
+                </div>
+                
+                <div className="space-y-10 md:space-y-0 md:flex md:space-x-36">
+                  <div className="space-y-4 w-6/12">
+                    <div className="space-y-2">
+                      <label className=" " htmlFor="avatar">更新照片</label>
+                      <input className="upload_img_input  md:w-full" type="file"
+                        id="avatar" name="avatar" accept="image/*"
+                        onChange={ handleImage }
+                        /> 
+                    </div>
+
+                    <div>
+                      <div>
+                        {previewImage !== null ?
+                          previewImage.map((item,index) => (
+                            <div className="space-y-2" key={index}>
+                              <img className="border rounded-lg h-32 w-32" src={item.preview} alt="預覽圖片" />
+                              <input type="button" value="刪除"
+                                className="btn-md btn-out cursor-pointer"
+                                onClick={()=>{removeImage(index)}}
+                              />  
+                            </div> 
+                          ))
+                        :
+                          null
+                        } 
+                      </div>
+                    </div>  
+                  </div>
+
+                  <div className="space-y-2 w-6/12">
+                    <div> 
+                      <span>更新資料</span>
+                    </div>
+                    <div className="space-y-2">
+
+                      <div className="flex items-center space-x-2">
+                        <label htmlFor="updatName">
+                          <BsFillPersonVcardFill size={24} color="#4b5563"/>
+                        </label>
+                        <input id="updatName" name="updatName" type="text" autoComplete="current-password"
+                          placeholder="會員姓名"
+                          value={formAccount.updatName}
+                          onChange={ handleDataChange }
+                          className="border-b focus:outline-none placeholder:text-sm"/>
+                      </div>
+                      <div className="flex items-center  space-x-2">
+                        <label htmlFor="newPassword">
+                          <BsLockFill size={24} color="#4b5563"/>
+                        </label>
+                        <input id="newPassword" name="newPassword" type="password" autoComplete="current-password"
+                          placeholder="新密碼請設定8碼英數字"
+                          value={formAccount.newPassword}
+                          onChange={ handleDataChange }
+                          className="border-b focus:outline-none placeholder:text-sm"/>
+                      </div>
+                      <div className="flex items-center  space-x-2">
+                        <label htmlFor="checkPassword">
+                          <LuShieldCheck size={24} color="#4b5563"/>
+                        </label>
+                        <input id="checkPassword" name="checkPassword" type="password" autoComplete="current-password"
+                          placeholder="請確認新密碼"
+                          value={checkPassword}
+                          onChange={e => setCheckPassword(e.target.value)}
+                          className="border-b focus:outline-none placeholder:text-sm"/>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+
+      </div>
+    </section>
   )
 }
 
